df457ee3f81002cafe27687e33c09a0b05df9a82
abhinav-973 <vabhinav898@gmail.com>
2026-09-21 13:40:09 +0530

    Remove generated files from repository

--- backend/db.js
+++ backend/db.js
@@ -4,6 +4,8 @@ const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI)
         console.log('Connected to MongoDB');
+        console.log("Database: ", mongoose.connection.name)
+        console.log("Host: ", mongoose.connection.host)
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
         process.exit(1);
